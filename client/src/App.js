9a41ffb64683df0ab74c8bcac3be0bd8a8cac6b2
Amogh Dalal <53583492+SilverArgente@users.noreply.github.com>
2025-03-21 21:56:36 -0400

    Init + bootstrap navbar template added
    Initialized project and added a basic navbar using bootstrap.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Landing from './components/Landing.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Landing() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">BetterU</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav float-center">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Sign Up</Nav.Link>
+            <Nav.Link href="#link">Log In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+  
+  export default Landing;
