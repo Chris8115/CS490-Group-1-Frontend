5953b4b02c1aee16d4016dcd007dcd3ccb4939bb
Amogh Dalal <53583492+SilverArgente@users.noreply.github.com>
2025-04-02 19:24:38 -0400

    added banner

--- client/src/app/routes/Landing.js
+++ client/src/app/routes/Landing.js
@@ -1,8 +1,19 @@
 import BetterUNavbar from "../../components/BetterUNavbar";
+import Divider from "../../components/Divider";
 
 function Landing() {
     return <>
         <BetterUNavbar/>
+
+        <div className="banner">
+            <div>
+                <h1>Achieve your <span className="special-text">dream body</span> - without the hassle.</h1>
+                <p>Browse from hundreds of doctors, specializing in fitness, weight loss, and nutrition, today.</p>
+            </div>
+            <button className="btn btn-primary make-account-btn">Make your account for <br/>free today</button>
+        </div>
+
+        <Divider/>
     </>
 }
 
---
+++ client/src/components/Divider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/custom.css';
+
+function Divider() {
+  return (
+    <div className="divider"></div>
+  );
+}
+  
+  export default Divider;
