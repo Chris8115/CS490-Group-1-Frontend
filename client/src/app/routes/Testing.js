a2f0a7d14fb6e5df2d901a95d57ccccc6d8df66c
Zain <sonicfanjackson@gmail.com>
2025-04-07 22:04:07 -0400

    Added textures

--- client/src/app/routes/Testing.js
+++ client/src/app/routes/Testing.js
@@ -3,7 +3,7 @@ import DashboardItem from "../../components/DashboardItem";
 
 function Testing() {
     return <>
-        <DashboardItem itemName="Write a Review" itemDescription="Fill out a review for your doctor."/>
+        <DashboardItem itemName="Write a Review" itemDescription="Fill out a review for your doctor." href="/patient/review" />
     </>
 }
 
--- client/src/components/DashboardItem.js
+++ client/src/components/DashboardItem.js
@@ -1,10 +1,18 @@
+import './DashboardItem.css'
+
 function DashboardItem(props) {
     return <>
-    
-        <div style={{ display: 'flex', flexDirection: "column", alignContent: "flex-start" }}>
-            <h2>{props.itemName}</h2>
-            <p>{props.itemDescription}</p>
+    <a href={props.href}>
+        <div style = {{ display: 'flex', flexDirection: "row", alignContent: "flex-start"}}>
+            <img src={  require( '../assets/DashboardTestIcon.png' )    } />
+
+            <div style={{ display: 'flex', flexDirection: "column", justifyContent: "center" }}>
+                <h2>{props.itemName}</h2>
+                <p>{props.itemDescription}</p>
+            </div>
+        
         </div>
+    </a>
 
     </>
 }
