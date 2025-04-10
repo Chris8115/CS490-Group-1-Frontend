2023a5a9925aa6230ed185a80a76104b0cc6bd6c
silver <53583492+SilverArgente@users.noreply.github.com>
2025-04-10 19:14:51 -0400

    basic login feature

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,25 +12,54 @@ function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [responseMessage, setResponseMessage] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
 
     const handleSubmit = async (event) => {
+
         event.preventDefault();
-    
+        console.log(email);
+        console.log(password);
         try {
-          const response = await fetch('/api/login', {
+          const response = await fetch(`http://localhost:5000/login?email=${email}&password=${password}`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
             body: JSON.stringify({ email, password }),
           });
-    
+          
           const data = await response.json();
+          console.log(data);
+          setLoggedIn(true);
+
           setResponseMessage(data.message);
         } catch (error) {
           setResponseMessage('An error occurred.');
+          console.log("An error occurred while logging in.");
         }
+
       };
+
+      useEffect(() => {
+        try {
+          const response = fetch(`http://localhost:5000/login_check`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          });
+          
+          const data = response.json();
+          console.log(data);
+
+          
+
+          setResponseMessage(data.message);
+        } catch (error) {
+          setResponseMessage('An error occurred.');
+          console.log("An error occurred to log in.");
+        }
+      }, [loggedIn])
 
     return (
 
@@ -38,19 +67,19 @@ function LoginForm() {
 
         <h1 className='login-title'>Sign into your account</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form>
 
             <label>Email Address</label>
-            <MDBInput wrapperClass='mb-4' label='' id='form1' type='email'/>
+            <MDBInput wrapperClass='mb-4' label='' id='form1' type='email'  onChange={(e) => setEmail(e.target.value)} />
             <label>Password</label>
-            <MDBInput wrapperClass='mb-4' label='' id='form2' type='password' onChange={(e) => setEmail(e.target.value)}/>
+            <MDBInput wrapperClass='mb-4' label='' id='form2' type='password' onChange={(e) => setPassword(e.target.value)}/>
 
             <div className="d-flex justify-content-between mx-3 mb-4">
                 <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
                 <a href="!#">Forgot password?</a>
             </div>
 
-            <button type='submit' className="login-btn">Sign in</button>
+            <button type='submit' className="login-btn" onClick={handleSubmit}>Sign in</button>
 
             <div className="text-center">
                 <p>Not a member? <a href="/sign-up">Register</a></p>
