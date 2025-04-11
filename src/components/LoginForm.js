7f7ab55e5eb0ec3a7150a0d5db0ed0d35537854c
Amogh Dalal <53583492+SilverArgente@users.noreply.github.com>
2025-04-10 22:59:51 -0400

    fixed login feature w/ blank redirect
    and session variable saving

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
     MDBContainer,
     MDBInput,
@@ -9,55 +10,55 @@ import {
 import '../css/custom.css';
 
 function LoginForm() {
+    
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [responseMessage, setResponseMessage] = useState('');
-    const [loggedIn, setLoggedIn] = useState(false);
+    const [loggedIn, setLoggedIn] = useState('first-time');
+    const [failedLogin, setFailedLogin] = useState(false);
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (event) => {
 
         event.preventDefault();
-        console.log(email);
-        console.log(password);
+        
         try {
-          const response = await fetch(`http://localhost:5000/login?email=${email}&password=${password}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-          });
-          
-          const data = await response.json();
-          console.log(data);
-          setLoggedIn(true);
 
-          setResponseMessage(data.message);
-        } catch (error) {
-          setResponseMessage('An error occurred.');
-          console.log("An error occurred while logging in.");
+            const response = await fetch(`http://localhost:5000/login?email=${email}&password=${password}`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ email, password }),
+
+            });
+    
+            if (!response.ok) throw new Error('Login failed');
+            
+            const data = await response.json();
+            
+            localStorage.setItem('email', data.email);
+            localStorage.setItem('user_id', data.user_id);
+            localStorage.setItem('role', data.role);
+            
+            setLoggedIn('success');
+            setResponseMessage(data.message);
+            console.log(data);
         }
-
+        catch (err) {
+          console.error(err);
+          setLoggedIn('failed');
+        }
+        
       };
-
+      
       useEffect(() => {
-        try {
-          const response = fetch(`http://localhost:5000/login_check`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            }
-          });
-          
-          const data = response.json();
-          console.log(data);
-
-          
-
-          setResponseMessage(data.message);
-        } catch (error) {
-          setResponseMessage('An error occurred.');
-          console.log("An error occurred to log in.");
+        if (loggedIn === 'success') { 
+          navigate('/dashboard_redirect');
+        }
+        else if (loggedIn === 'failed') {
+          setFailedLogin(true);
         }
       }, [loggedIn])
 
@@ -67,19 +68,21 @@ function LoginForm() {
 
         <h1 className='login-title'>Sign into your account</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
+
+          {failedLogin ? <p style={{color: 'red', fontSize: '18px'}}>Your username or password was incorrect.</p> : <></>}
 
             <label>Email Address</label>
-            <MDBInput wrapperClass='mb-4' label='' id='form1' type='email'  onChange={(e) => setEmail(e.target.value)} />
+            <MDBInput wrapperClass='mb-4' label='' id='form1' type='email' value={email} placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} />
             <label>Password</label>
-            <MDBInput wrapperClass='mb-4' label='' id='form2' type='password' onChange={(e) => setPassword(e.target.value)}/>
+            <MDBInput wrapperClass='mb-4' label='' id='form2' type='password' value={password} placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
 
             <div className="d-flex justify-content-between mx-3 mb-4">
                 <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
                 <a href="!#">Forgot password?</a>
             </div>
 
-            <button type='submit' className="login-btn" onClick={handleSubmit}>Sign in</button>
+            <button type='submit' className="login-btn" href=''>Sign in</button>
 
             <div className="text-center">
                 <p>Not a member? <a href="/sign-up">Register</a></p>
