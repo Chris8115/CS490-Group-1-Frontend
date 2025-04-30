2a01d7303aa20edd96f3129ed913ca432aefb3b6
ryanrnjit <rjr@njit.edu>
2025-04-29 22:10:53 -0400

    Styling, bugfixes.

--- src/app/routes/Forums.js
+++ src/app/routes/Forums.js
@@ -4,8 +4,23 @@ import BetterUNavbar from '../../components/BetterUNavbar';
 import { Divider } from '@mui/material';
 import Footer from '../../components/Footer';
 import ForumPosts from './Forums/ForumPosts.js';
+import { useNavigate } from 'react-router-dom';
 
 function Forums() {
+    const navigate = useNavigate();
+    useEffect(()=>{
+        fetch('/api/betteru/login_check', {
+            method: 'GET',
+            credentials: 'include',
+            redirect: 'manual', /* Needed for login_check */
+        }).then(resp => {
+            console.log(resp.status);
+            if(resp.status != 200) {
+                alert("You need to be logged in to view the forums.");
+                navigate('/log-in');
+            }
+        })
+    }, []);
     return (
         <>
             <BetterUNavbar />
