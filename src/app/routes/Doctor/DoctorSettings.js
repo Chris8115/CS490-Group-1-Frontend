8fc107c604eb38c8ad0e20d71a231e44152fc4f9
ezhang <ericzhang200@gmail.com>
2025-04-29 23:48:13 -0400

    fixed saving new meeting location

--- src/app/routes/Doctor/DoctorSettings.js
+++ src/app/routes/Doctor/DoctorSettings.js
@@ -64,12 +64,12 @@ function DoctorSettings() {
         setEditingLocation(false);
 
         try {
-            await fetch(`/api/betteru/doctors/location/${user_info.user_id}`, {  // <-- Adjust if needed
+            await fetch(`/api/betteru/doctors/${user_info.user_id}`, {  // <-- Adjust if needed
                 method: "PATCH",
                 headers: { "Content-Type": "application/json" },
                 credentials: 'include',
                 body: JSON.stringify({
-                    location: location
+                    office: location
                 }),
             });
         } catch (err) {
