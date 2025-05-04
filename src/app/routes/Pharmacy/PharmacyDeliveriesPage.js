a1a3c58896c6e8fe68edbd9f4312f20ed59b20d2
ZainWD <zainaabbas347@gmail.com>
2025-05-04 09:53:41 -0400

    Can now accept/reject deliveries

---
+++ src/app/routes/Pharmacy/PharmacyDeliveriesPage.js
@@ -0,0 +1,14 @@
+import PharmacyDeliveries from "../../../components/PharmacyDeliveries";
+
+function PharmacyDeliveriesPage() {
+ 
+    return (
+        <>        
+        <h1>Deliveries</h1>   
+        <PharmacyDeliveries/>
+    
+        </>
+    )
+}
+
+export default PharmacyDeliveriesPage;
